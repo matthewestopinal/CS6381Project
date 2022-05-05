144f1960376b50d90df9af8b9603103ec16c81ec
matthewestopinal <53093042+matthewestopinal@users.noreply.github.com>
2022-05-05 16:27:24 -0500

    Split RL Model into it's own file
    Want to separate it out just to get ahead of having to deal with potential merge conflicts.

---
+++ RLScheduler.py
@@ -0,0 +1,9 @@
+from scheduler import Scheduler
+
+#DeepQLearning Scheduler
+#TODO: Implement the deep learning model
+#Will want to store the model as a class variable
+class RLScheduler(Scheduler):
+
+    def schedule_job(self, clusters, job):
+        pass
